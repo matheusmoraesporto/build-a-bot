61052c2f467862ae49bbd4fb9c03335bfcf930f4
Matheus Porto <matheus.porto@sap.com>
2023-10-04 11:09:27 -0300

    Disable max-len linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,6 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'linebreak-style': 0,
     'operator-linebreak': 0,
+    'max-len': 'off',
   },
 };
