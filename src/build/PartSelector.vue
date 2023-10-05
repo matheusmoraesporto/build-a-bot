cf6a922adcc96c2577aa3ee569322c41ac907c62
Matheus Porto <matheus.porto@sap.com>
2023-10-05 16:03:50 -0300

    Add props validators

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -11,8 +11,19 @@
 <script setup>
 import { computed, ref } from 'vue';
 
-const props = defineProps(['parts', 'position']);
-
+const props = defineProps({
+  parts: {
+    type: Array,
+    required: true,
+  },
+  position: {
+    type: String,
+    required: true,
+    validator(value) {
+      return ['left', 'right', 'top', 'center', 'bottom'].includes(value);
+    },
+  },
+});
 const selectedPartIndex = ref(0);
 const selectedPart = computed(() => props.parts[selectedPartIndex.value]);
 
